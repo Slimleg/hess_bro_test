5d3bae931ce4c10f4e60bb8e446d0c66a048aaf1
Hunter Hess <hunterhess7@gmail.com>
2024-06-17 16:24:43 -0700

    Added comments to js file

--- Game_Class.js
+++ Game_Class.js
@@ -1,3 +1,4 @@
+//Game class that can construct games
 class Game {
     constructor(name, genre, rating) {
         this.name = name;
@@ -5,13 +6,13 @@ class Game {
         this.rating = rating;
     }
 }
-
+//All the games concstructed by the Game class
 var starCraft = new Game("StarCraft", "RTS", "Mature");
 var l4d2 = new Game("Left 4 Dead 2", "FPS/Zombie", "Mature");
 var overWatch = new Game("Overwatch 2", "MOBA", "Teen");
-
+//An array that all the games fit into
 var gameCollection = [starCraft, l4d2, overWatch];
-
+//A function that when called can log the information of x into the console, x being a game selected from the array
 function pickGame(x) {
     if (x == gameCollection[0]) {
         console.log(gameCollection[0].name, gameCollection[0].genre, gameCollection[0].rating);
@@ -23,7 +24,7 @@ function pickGame(x) {
         console.log("Error, game not found.");
     }
 }
-
+//Calling the pickGame function 
 pickGame(gameCollection[0]);
 pickGame(gameCollection[1]);
 pickGame(gameCollection[2]);
