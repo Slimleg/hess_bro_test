183511257c3d6344b10a475924522f905b94dbbf
Hunter Hess <hunterhess7@gmail.com>
2024-06-17 16:14:25 -0700

    js file update

--- Game_Class.js
+++ Game_Class.js
@@ -16,12 +16,17 @@ function pickGame(x) {
     if (x == gameCollection[0]) {
         console.log(gameCollection[0].name, gameCollection[0].genre, gameCollection[0].rating);
     } else if (x == gameCollection[1]) {
-        console.log(gameCollection[1]);
+        console.log(gameCollection[1].name, gameCollection[1].genre, gameCollection[1].rating);
     } else if (x == gameCollection[2]) {
-        console.log(gameCollection[2]);
+        console.log(gameCollection[2].name, gameCollection[2].genre, gameCollection[2].rating);
     } else {
         console.log("Error, game not found.");
     }
 }
 
 pickGame(gameCollection[0]);
+pickGame(gameCollection[1]);
+pickGame(gameCollection[2]);
+
+
+
